a3abbae5eebed9569382856921e3464181dcf5e0
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-30 20:48:35 +0300

    returns a reversed string

--- solution1.py
+++ solution1.py
@@ -1,7 +1,6 @@
 def add_numbers(num1, num2):
      return num1 + num2
-print(add_numbers(2,4))  
-
+print(add_numbers(2,4) )      
 def is_even(number):
     if number % 2 ==0:
          return True
@@ -9,3 +8,15 @@ def is_even(number):
          return False
 print(is_even(30))    
 print(is_even(33))    
+
+
+def reverse_string(text):
+    #  reverse the input (text)
+     reversed_text = "".join(reversed(text))
+     return reversed_text
+    
+print(reverse_string("word"))     
+print(reverse_string("race car here"))     
+
+
+ 
