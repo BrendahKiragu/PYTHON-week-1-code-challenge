d247698fab7e3248e0d4c8c07c67bfc974249b31
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-30 20:47:46 +0300

    return true for even numbers and false for odd numbers

--- solution1.py
+++ solution1.py
@@ -1,3 +1,11 @@
 def add_numbers(num1, num2):
      return num1 + num2
-print(add_numbers(2,4) )      
+print(add_numbers(2,4))  
+
+def is_even(number):
+    if number % 2 ==0:
+         return True
+    else:
+         return False
+print(is_even(30))    
+print(is_even(33))    
