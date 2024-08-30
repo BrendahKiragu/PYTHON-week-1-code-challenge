43c9092308d201976212200e3cb9e55a6186be11
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-30 20:50:42 +0300

    returns factorial of a number

--- solution1.py
+++ solution1.py
@@ -1,6 +1,8 @@
 def add_numbers(num1, num2):
      return num1 + num2
-print(add_numbers(2,4) )      
+print(add_numbers(2,4) ) 
+
+
 def is_even(number):
     if number % 2 ==0:
          return True
@@ -27,4 +29,16 @@ def count_vowels(text):
 print(count_vowels('python is a cool language'))       
 
 
-
+def calculate_factorial(n):
+    if n < 0:
+         return "Factorial not applicable to negative numbers"
+    elif n == 0:
+         return 1
+    
+    factorial = 1
+  # iterates thru numbers from 1 to n and multiplies each i with the current value of factorial
+    for i in range (1, n+1):
+        factorial = factorial * i
+    return factorial  
+   
+print(calculate_factorial(5))     
