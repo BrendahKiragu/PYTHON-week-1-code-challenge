6425fc8094482fe30e89e9d8e3521ccb2121ce59
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-31 13:10:44 +0300

    updates comments for each function

--- solution1.py
+++ solution1.py
@@ -1,20 +1,22 @@
-# takes two parameters num1 and num2, and returns the sum of the two numbers.
+# takes two parameters (num1 and num2), and returns the sum of the two numbers.
 def add_numbers(num1, num2):
      return num1 + num2
+
 print(add_numbers(2,4) ) 
 
 
-# takes a single parameter number and returns True if the number is even, and False otherwise.
+# takes a single parameter (number) and returns True if the number is even, and False if otherwise.
 def is_even(number):
     if number % 2 ==0:
          return True
     else:
          return False
+    
 print(is_even(30))    
 print(is_even(33))    
 
 
-# takes a string text as input and returns the reversed version of that string.
+# takes a string (text) as input and returns the reversed version of that string.
 def reverse_string(text):
     #  reverse the input (text)
      reversed_text = "".join(reversed(text))
