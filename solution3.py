8718fce25050f379a575753bb2113a37decbdc8e
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-31 13:01:26 +0300

    updates comment for dict merging function

--- solution3.py
+++ solution3.py
@@ -2,15 +2,15 @@
 def sort_by_age(sample_tuple):
       return sorted(sample_tuple, key=lambda a: a[1])
 
-print(sort_by_age([('bree', 5), ('mark', 3), ('james', 1)]))
+print(sort_by_age([('Bree', 15), ('Grace', 20), ('Mark', 3), ('James', 1)]))
 
 
-#  sorts the list of tuples by age in ascending order.
+#  merges two dictionaries
 def merge_dicts(dict1, dict2):
     merged_dict = {**dict1, **dict2}
     return merged_dict
 
-print(merge_dicts({'name':'bree'}, {'occupation': 'devops'}))
+print(merge_dicts({'Name':'Bree'}, {'Occupation': 'Devops engineer'}))
 
 
 # creates class Car with a attributes a method that prints the car details
